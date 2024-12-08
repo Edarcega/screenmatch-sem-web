044698dfa133c52a4ae378a4f9f53f5c6eaa49e8
edimar <edimar.eds@gmail.com>
2024-12-08 11:34:01 -0300

    Implementando buca pelo nome da serie e separando as responsabilidades dos métodos

---
+++ src/main/java/br/com/alura/screenmatch/principal/Principal.java
@@ -0,0 +1,44 @@
+package br.com.alura.screenmatch.principal;
+
+import br.com.alura.screenmatch.model.DadosSerie;
+import br.com.alura.screenmatch.model.DadosTemporada;
+import br.com.alura.screenmatch.services.ConsumoAPI;
+import br.com.alura.screenmatch.services.ConverterDados;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+@Component
+public class Principal {
+    Scanner sc = new Scanner(System.in);
+
+    @Autowired
+    private ConsumoAPI consumoAPI;
+
+    @Autowired
+    private ConverterDados converterDados;
+
+    public final String ENDERECO = "https://www.omdbapi.com/?t=";
+
+    public final String API_KEY = "&apikey=6585022c";
+
+    public void exibeMenu() {
+        System.out.println("Digite o nome da série para buscar");
+        var nomeSerie = sc.nextLine();
+        var json = consumoAPI.obterDados(ENDERECO + nomeSerie.replace(" ", "+") + API_KEY);
+        DadosSerie dadosSerie = converterDados.obterDados(json, DadosSerie.class);
+        System.out.println(dadosSerie);
+
+        List<DadosTemporada> temporadas = new ArrayList<>();
+        for (int i = 1; i <= dadosSerie.totalTemporadas(); i++) {
+            json = consumoAPI.obterDados(ENDERECO + nomeSerie.replace(" ", "+") + "&season=" + i + API_KEY);
+            DadosTemporada dadosTemporada = converterDados.obterDados(json, DadosTemporada.class);
+            temporadas.add(dadosTemporada);
+        }
+        temporadas.forEach(System.out::println);
+
+    }
+}
